5d511bb48b1afb416267fb5de2211d168e640943
Sazzad Hossain <sazzad362@gmail.com>
2019-07-15 10:58:20 +0600

    Git init after merge all files

---
+++ js/app.js
@@ -0,0 +1,49 @@
+// IIFE with jQuery Wrapper
+(function($) {
+  'use strict';
+
+  /*
+   *----------------------------------
+   * Document Ready
+   *----------------------------------
+   */
+	$(document).ready(function() {
+
+	    /****************************
+	      stellarnav DropDown Menu
+	    *****************************/
+	    // $('.stellarnav').stellarNav({ position: 'right' });
+	    // var gsMagaMenu = $('#gs-maga-menu').innerWidth();
+	    // $('#gs-maga-menu .gs-maga ul')
+	    // .css("width", gsMagaMenu);
+
+
+
+	    /****************************
+	      stellarnav DropDown Menu
+	    *****************************/
+	    // $('.owl-carousel').owlCarousel({
+	    //     loop:true,
+	    //     margin:0,
+	    //     nav:true,
+	    //     items:1,
+	    //     responsive:{
+	    //         0:{
+	    //             items:1
+	    //         },
+	    //         600:{
+	    //             items:1
+	    //         },
+	    //         1000:{
+	    //             items:1
+	    //         }
+	    //     }
+	    // })
+
+
+	    
+	});// DOM Ready
+
+
+}(jQuery)); // IIFE
+
